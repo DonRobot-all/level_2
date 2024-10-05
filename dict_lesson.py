c4c1d4dd41a430d1d5dbd51ab82475540a2c3147
Ilya <pankovskiy.im@gmail.com>
2024-10-05 19:31:31 +0300

    new tasks

--- dict_lesson.py
+++ dict_lesson.py
@@ -1,1 +1,7 @@
-dict = {}
+scrumblecost={
+    ('A','E','I','O','U','L','N','S','T','R'):1,
+    ('D','G'):2,
+}
+for chars, i in scrumblecost.items():
+    print(chars)
+
---
+++ dict_task3.py
@@ -0,0 +1,14 @@
+emails = {'mgu.edu': ['andrei_serov', 'alexander_pushkin', 'elena_belova', 'kirill_stepanov'],
+      	'gmail.com': ['alena.semyonova', 'ivan.polekhin', 'marina_abrabova'],
+      	'msu.edu': ['sergei.zharkov', 'julia_lyubimova', 'vitaliy.smirnoff'],
+      	'yandex.ru': ['ekaterina_ivanova', 'glebova_nastya'],
+      	'harvard.edu': ['john.doe', 'mark.zuckerberg', 'helen_hunt'],
+      	'mail.ru': ['roman.kolosov', 'ilya_gromov', 'masha.yashkina']}
+
+
+all_address = []
+for email, nicknames in emails.items():
+    for nickname in nicknames:
+        all_address.append(nickname + '@' + email)
+
+print(all_address)
