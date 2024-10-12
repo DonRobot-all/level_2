dd3dd3bbe90d5293cde691e457262bcbb2ca3d74
Ilya <pankovskiy.im@gmail.com>
2024-10-12 21:16:58 +0300

    new task

---
+++ learningapps_correct_cycles.py
@@ -0,0 +1,34 @@
+t_dict = {'яблоки': 100, 'бананы': 333, 'груши': 200}
+t_string = 'яблоки'
+t_list = ['яблоки', 'бананы', 'груши']
+t_set = ('яблоки', 'бананы', 'груши')
+
+# for key, value in t_dict.key():
+#     ...
+
+for key, value in t_dict.items():
+    ...
+
+for value in t_dict.values():
+    ...
+
+for idx in range(len(t_string)):
+    ...
+
+# for idx, value in t_string:
+#     ...
+
+for idx, value in enumerate(t_list):
+    ...
+
+for value in t_list:
+    ...
+
+# for idx in t_set.index():
+#     ...
+
+for value in t_set:
+    ...
+
+
+
